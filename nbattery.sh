b99b70f6959d7afe2f48931e574260093ebd5679
reDpz <sv_redpz@proton.me>
2023-10-18 00:52:37 +0100

    Additional icon changes. Added battery percentage

--- nbattery.sh
+++ nbattery.sh
@@ -28,7 +28,17 @@ _get_battery_status() {
         printf 󰁹
     fi
 
-    [ "$_current_status" = "Charging" ] && printf   
+    # Here I've changed the charging symbol from  to 󱐋 because the latter is much more readable
+    if [ "$_current_status" = "Charging" ]; then
+        printf 󱐋
+    # this else statement just makes sure that the spacing between the battery number and the battery icon stays consistent
+    else
+        printf " "
+    fi
+
+    # another small modification to just allow me to see the current battery status, I thought that if this program is already fetching and storing the value there is no need for me to run a seperate command to see it.
+    # If you dont want it you can disable it by commenting it out with a "#"
+    printf "$_current_capacity"
 }
 
 _get_battery_status
